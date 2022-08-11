4754cecdedfa3262586a92932821f417ee96fdc8
Charan <charanlucky6143@gmail.com>
2022-08-11 19:02:01 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(el);
+
+function App() {
+    // let message = 'hello react';
+    // if (Math.random() > 0.5) {
+    //     message = 'bye recat'
+    // }
+    // const date = new Date();
+    // const time = date.toLocaleTimeString();
+    // return <h1>{new Date().toLocaleTimeString()}</h1>;
+    // return <h1>{message},<br /> I'm new here!</h1>;
+    return (
+        <div>
+            <h1>Enter Your Age:-</h1>
+            <input placeholder="hi there" />
+            {/* <input type="number" min={5} style={{ color: 'red' }} /> */}
+        </div>
+    );
+}
+
+root.render(<App />);
